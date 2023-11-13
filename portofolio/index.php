119c71ee110c1e73047b0ce86205c0c721c72bc2
Akhmad fa'iq ismail <112614163+faiqismail@users.noreply.github.com>
2023-11-13 15:57:01 +0700

    upload

---
+++ portofolio/index.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portofolio Saya</title>
+  
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        .navbar {
+            margin-bottom: 20px;
+        }
+
+        .profile-image {
+            max-width: 200px;
+            border: 5px solid #fff;
+            border-radius: 50%;
+        }
+
+        .card {
+            margin-bottom: 20px;
+        }
+        body {
+            background-image: url('https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/rm222-mind-16_1.jpg?w=800&dpr=1&fit=default&crop=default&q=65&vib=3&con=3&usm=15&bg=F4F4F3&ixlib=js-2.2.1&s=1c53e59a7b21c1ffee85bc36a3837392'); /* Ganti dengan lokasi gambar latar belakang Anda */
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+        }
+    </style>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand">Portofolio</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#home">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#portfolio">Portofolio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#contact">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container mt-5" id="home">
+        <div class="row">
+            <div class="col-md-4 text-center">
+                <img src="foto/img 2.jpg" alt="Foto Profil" class="img-fluid profile-image">
+            </div>
+            <div class="col-md-8">
+                <h2>Akhmad Fa'iq Ismail</h2><br>
+                <p> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Selamat datang di portofolio saya. Saya adalah seorang Mahasiswa di politeknik elektronika negeri surabaya (PENS) dengan prodi D3 Teknik informatika sekarang sudah semester 3  keahlian saya sesuai portofolio dan masih banyak lagi dengan elektronika dengan sertifikat BNSP dan lainya </p>
+            </div>
+        </div>
+    </div>
+
+
+
+<div class="container mt-5" id="portfolio">
+    <h1 style="text-align:center;">PORTOFOLIO</h1><br>
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card">
+                <img src="foto/Screenshot 2023-06-01 111835.png" class="card-img-top" alt="Proyek 1">
+                <div class="card-body">
+                    <h5 class="card-title">SERTIFIKAT</h5>
+                    <p class="card-text">di dapatkan selama 3 bulan </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <img src="foto/Screenshot 2023-07-08 160102.png" class="card-img-top" alt="Proyek 2">
+                <div class="card-body">
+                    <h5 class="card-title">SERTIFIKAT</h5>
+                    <p class="card-text">di dapatkan selama 1 hari penuh </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <img src="foto/Screenshot 2023-06-30 190122.png" class="card-img-top" alt="Proyek 3">
+                <div class="card-body">
+                    <h5 class="card-title"> APLIKASI LAYANAN </h5>
+                    <p class="card-text">di kerjakan selama 1 minggu </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <img src="foto/Screenshot 2023-08-28 191655.png" class="card-img-top" alt="Proyek 4">
+                <div class="card-body">
+                    <h5 class="card-title">SERTIFIKAT</h5>
+                    <p class="card-text"> di  dapatkan selama 1 hari</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <img src="foto/Screenshot 2023-08-28 191754.png" class="card-img-top" alt="Proyek 5">
+                <div class="card-body">
+                    <h5 class="card-title">SERTIFIKAT</h5>
+                    <p class="card-text"> di dapatkan selama 2 bulan </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <img src="foto/Screenshot 2023-08-28 191735.png" class="card-img-top" alt="Proyek 6">
+                <div class="card-body">
+                    <h5 class="card-title">SERTIFIKAT</h5>
+                    <p class="card-text"> di dapatkan selama 2 hari</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<div class="container mt-5" id="contact">
+    <h2 style="text-align:center;">Contact</h2><br>
+    <p style="text-align:center;">
+        <img src="https://cdn4.iconfinder.com/data/icons/social-media-logos-6/512/112-gmail_email_mail-512.png" alt="Logo Email" width="30" height="30">
+        Untuk menghubungi saya, Anda dapat mengirim email ke: 
+        <a href="mailto:faiq69bikejr@gmail.com">faiq69bikejr@gmail.com</a>
+    </p>
+    <p style="text-align:center;">
+        <img src="https://www.tanjunglesung.com/wp-content/uploads/2018/12/logo-wa-whatsapp.png" alt="Logo WhatsApp" width="30" height="30">
+        Atau menghubungi melalui WhatsApp di: 
+        <a href="https://wa.me/085155476677">085155476677</a>
+    </p>
+</div>
+
+<br><br><br><br>
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
